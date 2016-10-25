b32109b8a06e981a2dfe66e41ebf4d23cd967440
James <James.Steel@me.com>
2016-10-25 15:07:08 +0100

    added weather temperature, minor refactor of weather interface

--- js/interface.js
+++ js/interface.js
@@ -31,15 +31,22 @@ $(document).ready(function() {
   })
 
   $('#current-city').change(function() {
-  var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=f02bb424d7c89aa13e065244a6a6485f&units=metric', function(data) {
-      $('#current-temperature').text(data.main.temp);
-    })
+    var city = $('#current-city').val();
+    showWeather(city)
   })
 
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.displayColour());
+  }
+
+  function showWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=f02bb424d7c89aa13e065244a6a6485f';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
   }
 })
