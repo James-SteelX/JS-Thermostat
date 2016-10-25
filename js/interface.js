8f1dc1e582a4c7669fb7dd7433bcb6d145df25fb
James <James.Steel@me.com>
2016-10-25 12:48:20 +0100

    added JQuery, everything now functions on the page

--- js/interface.js
+++ js/interface.js
@@ -1,15 +1,39 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
-  $('#temperature').text(thermostat.temperature);
+  updateTemperature();
 
   $('#temperature-up').on('click', function() {
     thermostat.increaseTemperature();
-    $('#temperature').text(thermostat.temperature);
+    updateTemperature();
   })
 
   $('#temperature-down').on('click', function() {
     thermostat.decreaseTemperature();
-    $('#temperature').text(thermostat.temperature); 
+    updateTemperature();
   })
+
+  $('#temperature-reset').on('click', function(){
+    thermostat.resetTemperature();
+    updateTemperature();
+  })
+
+  $('#powersaving-on').on('click', function(){
+    thermostat.turnOnPowerSaving();
+    $('#power-saving-status').text('on')
+    updateTemperature();
+  })
+
+  $('#powersaving-off').on('click', function(){
+    thermostat.turnOffPowerSaving();
+    $('#power-saving-status').text('off')
+    updateTemperature();
+  })
+
+
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.displayColour());
+  }
 })
